a8ea304218d1ed138ec7a46fd47e1facc2a32681
Robin Rolf <imer@imer.cc>
2025-05-17 13:47:57 +0000

    feat: HtmlString concat helper

--- src/HtmlString.php
+++ src/HtmlString.php
@@ -10,9 +10,23 @@ class HtmlString implements Htmlable {
     public function __construct(string $string) {
         $this->string = $string;
     }
+
     public function __toString(): string {
         return $this->string;
     }
+
+    public static function concat($a, $b): HtmlString {
+        if ($a instanceof HtmlString) {
+            if ($b instanceof HtmlString) {
+                return new HtmlString($a->string . $b->string);
+            }
+            return new HtmlString($a->string . e($b));
+        } else if ($b instanceof HtmlString) {
+            return new HtmlString(e($a) . $b->string);
+        }
+        return new HtmlString(e($a) . e($b));
+    }
+
 
     /**
      * @inheritDoc
